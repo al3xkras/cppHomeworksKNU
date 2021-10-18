8b1e4e5f11099268e5c770b3741f1809c9256a26
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-10-18 14:44:00 +0300

    Edit HW9.2

--- Homework9.2/main.cpp
+++ Homework9.2/main.cpp
@@ -65,8 +65,65 @@ void task9_9_n(){
     delete(new_matrix);
 }
 
+void task9_9_n_2(){
+    size_t N, M;
+
+    cout<<"Input N>0: ";
+    scanf("%d",&N);
+
+    cout<<"Input M>0: ";
+    scanf("%d",&M);
+
+    if (N<=0 | M<=0) return;
+
+    int* matrix;
+    int* new_matrix;
+
+    int current_i=0;
+    size_t new_N = N/2;
+
+    matrix= (int*)calloc(N*M,sizeof(int));
+    new_matrix = (int*)calloc(new_N*M,sizeof(int));
+
+    for (int i=0; i<N*M; i++){
+        matrix[i]=randint(0,100);
+    }
+
+    
+    printf("\nMatrix %dx%d:\n",N,M);
+    for (int i=0; i<N*M; i=i+M){
+        cout<<i/M+1<<") [ ";
+
+        if (i%2!=0) {
+            for (int j=0; j<M;j++){
+                cout<<matrix[i+j]<<", ";
+                new_matrix[current_i++] = matrix[i+j];
+            }
+        } else {
+            for (int j=0; j<M;j++){
+                cout<<matrix[i+j]<<", ";
+            }
+        }
+
+        cout<<"]"<<endl;
+    }
+
+    printf("\nMatrix with dropped rows:\nMatrix %dx%d:\n",new_N,M);
+    for (int i=0; i<new_N*M; i=i+M){
+        cout<<2*i/M+2<<") [ ";
+        for (int j=0; j<M;j++){
+            cout<<new_matrix[i+j]<<", ";
+        }
+        cout<<"]"<<endl;
+    }
+
+    delete(matrix);
+    delete(new_matrix);
+}
+
 int main(){
     //11 Красовський Олександр 9.09н
     task9_9_n();
+    task9_9_n_2();
     return 0;
 }
