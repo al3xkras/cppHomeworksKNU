dba98f67c08d5fd55fd6f3610bcc3427b16b1e3e
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-11-04 12:06:58 +0200

    HW15: create Point2D,Line2D, Triangle classes, Line2D: length, center, intersection

--- Homework15/main.cpp
+++ Homework15/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-#include "/src/Point2D.h"
-#include "/src/Line2D.h"
-#include "/src/Triangle.h"
+#include "src/Point2D.h"
+#include "src/Line2D.h"
+#include "src/Triangle.h"
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Point2D a(0,0),
+            b(1,2),
+            c(0,-0.1),
+            d(1,0);
+    //std::cout<<"";
+    //a.input();
+    //b.input();
+    //c.input();
+    //d.input();
+
+    Line2D line1(a,b);
+    Line2D line2(c,d);
+
+    std::cout<<line1.intersection(line2);
+
     return 0;
 }
